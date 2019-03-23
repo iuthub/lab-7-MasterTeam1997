c65fa5a94999eada6afa89508aa97ca34b2c2b49
Ruslan Musyaev <musyaev@gmail.com>
2019-03-23 16:50:54 +0500

    Update imdb_queries.sql

--- sql/imdb_queries.sql
+++ sql/imdb_queries.sql
@@ -2,11 +2,21 @@ SELECT name FROM movies WHERE year = 1995
 Apollo 13
 Braveheart
 
+
+
+
+
+
 SELECT COUNT(*) FROM actors a 
 				JOIN roles r ON a.id = r.actor_id
 				JOIN movies m ON r.movie_id = m.id
 				WHERE m.name = 'Lost in Translation'
 51
+
+
+
+
+
 
 SELECT a.first_name, a.last_name FROM actors a 
 									JOIN roles r ON a.id = r.actor_id
@@ -65,17 +75,32 @@ Akiko Takeshita
 Lisle Wilkerson
 
 
+
+
+
+
+
 SELECT d.first_name, d.last_name FROM directors d
 								JOIN movies_directors md ON d.id = md.director_id
 								JOIN movies m ON m.id = md.movie_id
                                 WHERE m.name = 'Fight Club'
 David Fincher
 
+
+
+
+
+
 SELECT COUNT(*) FROM movies m
 				JOIN movies_directors md ON m.id = md.movie_id
 				JOIN directors d ON d.id = md.director_id
 				WHERE d.first_name = 'Clint' AND d.last_name = 'Eastwood'
 1
+
+
+
+
+
 
 SELECT m.name FROM movies m 
 				JOIN movies_directors md ON md.movie_id = m.id
